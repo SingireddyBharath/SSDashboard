ecfad68e835499951dd36f3ed3cdd99592f57526
Niranjan <niranjanreddy8688@gmail.com>
2023-08-16 12:08:41 +0530

    Login validation and clearing of data while pressing submit is completed auth gaurds have been added for future validation

--- SSDashboard/src/app/app-routing.module.ts
+++ SSDashboard/src/app/app-routing.module.ts
@@ -2,10 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './components/login/login.component';
 import { NotFoundComponent } from './components/not-found/not-found.component';
+import { authGuard } from './gaurds/auth.guard';
 
 const routes: Routes = [
   {path:'login',component:LoginComponent},
   {path:'',redirectTo:'/login',pathMatch:'full'},
+  {path:'admin',canActivate:[authGuard],
+  loadChildren:()=> import('./modules/admin/admin.module').then((admin)=>admin.AdminModule)},
   {path:'**',component:NotFoundComponent}
   
 ];
