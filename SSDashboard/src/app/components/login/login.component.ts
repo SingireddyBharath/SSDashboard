ccfd373956463b9dc5e231135f9e3ee3a4443b5e
Singireddy Bharath <singireddybharath99@gmail.com>
2023-08-16 16:29:52 +0530

    add service call and handled in login comp

--- SSDashboard/src/app/components/login/login.component.ts
+++ SSDashboard/src/app/components/login/login.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import {FormControl , FormGroup} from '@angular/forms'
+import { FormControl, FormGroup } from '@angular/forms'
 import { AppService } from 'src/app/services/app.service';
 
 @Component({
@@ -8,11 +8,14 @@ import { AppService } from 'src/app/services/app.service';
   styleUrls: ['./login.component.css'],
 })
 export class LoginComponent {
-  UserName : any = ''
-  password : any = ''
-  Login(item:any){
-    console.log(item)
-    this.UserName =''
-    this.password =''
+  constructor(private appService: AppService) { }
+  userName: any = ''
+  password: any = ''
+  Login(details: any) {
+    this.appService.getUserDetails().subscribe((res) => {
+      const data = res[0]
+      const isValid = (data.userName === details.userName && data.password === details.password)
+      console.log(isValid);
+    })
   }
 }
--- SSDashboard/src/app/services/app.service.ts
+++ SSDashboard/src/app/services/app.service.ts
@@ -1,14 +1,14 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AppService {
-  constructor() {}
-
-  static check(details:any):void{
-    if(details.UserName == "admin" && details.password == "Kore@123"){
-      console.log(true)
-    }
+  constructor(private http: HttpClient) {}   
+  getUserDetails(): Observable<any> {
+    return this.http.get<any>('https://6380f4bf786e112fe1bf0f9a.mockapi.io/users');
   }
 }
+
