65dee228af4b3bfeebc3711601825a8702189ab2
Singireddy Bharath <singireddybharath99@gmail.com>
2023-08-24 13:56:20 +0530

    updated packages

--- src/app/modules/admin/components/header/header.component.ts
+++ src/app/modules/admin/components/header/header.component.ts
@@ -1,19 +1,19 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+
 import { AppService } from 'src/app/services/app.service';
+
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent {
+  showCustomWindow = false;
   constructor(private router: Router, private appService: AppService) { }
-  showCustomWindow: boolean = false;
   logOut(): void {
-    console.log("called logout");
-    this.showCustomWindow = true
+    this.showCustomWindow = true;
     this.appService.setAuth(false);
     this.router.navigate(["/login"]);
   }
-
 }
