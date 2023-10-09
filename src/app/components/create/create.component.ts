cd85224f30eadcb5f538a50f0edbecc70bb6560c
Singireddy Bharath <singireddybharath99@gmail.com>
2023-10-09 23:46:35 +0530

    changed done

--- src/app/components/create/create.component.ts
+++ src/app/components/create/create.component.ts
@@ -1,4 +1,4 @@
-import { FormBuilder, Validators, ValidationErrors } from '@angular/forms';
+import { FormBuilder, Validators, ValidationErrors, ValidatorFn, AbstractControl } from '@angular/forms';
 import { FormGroup, FormControl } from '@angular/forms';
 import { COMMA, ENTER } from '@angular/cdk/keycodes';
 import { Component, OnInit } from '@angular/core';
@@ -16,8 +16,8 @@ import { MatSnackBar } from '@angular/material/snack-bar';
 
 export class CreateComponent implements OnInit {
 
-  topKvalue: number = 4;
-  isLinear = false;
+  topKvalue: number = 0;
+  isLinear = true;
   max = 10;
   min = 0;
   showTicks = false;
@@ -26,6 +26,10 @@ export class CreateComponent implements OnInit {
   announcer: any;
   addOnBlur = true;
   finalIndexJSON: any = {}
+  showFinetuneModel = false;
+  showFollowupModel = false;
+  showFallbackModel = false;
+  showPresentationModel = false;
   readonly separatorKeysCodes = [ENTER, COMMA] as const;
   finalJSON = JSON.stringify(this.finalIndexJSON, null, 2);
   instanceTypes: any[] = [
@@ -41,20 +45,24 @@ export class CreateComponent implements OnInit {
     "RA_Pinecone_LG",
     "Add Custom Model"
   ];
+  availableCustomModels = [
+    "Fallback Model",
+    "Presentation Model",
+    "Followup Model",
+    "Finetune Model"
+  ]
   pineconeCredentials!: FormGroup;
   searchPreferenceForm!: FormGroup;
   llmSpecification!: FormGroup;
 
   availableOptions: string[] = [
     'CX_INIT_PROMPT',
-    'FOLLOW_UP_QUERY_PROMPT',
-    'CONSOLIDATE_QUERY_PROMPT',
-    'SKU_SPECIFIC_PROMPT'
+    'SKU_SPECIFIC_PROMPT',
+    'CONSOLIDATE_QUERY_PROMPT'
   ];
-  availableModels: string[] = ["embeddingModel", "consolidatedQueryModel", "finetunedModel"]
-  ignorableKeys: Item[] = [{ name: 'example key' }];
-  namespaces: Item[] = [{ name: 'example namespace' }];
-
+  availableModels: string[] = ["embeddingModel", "consolidatedQueryModel"]
+  ignorableKeys: Item[] = [];
+  namespaces: Item[] = [];
 
   selectedOptions: { option: string, prompt: string }[] = [];
 
@@ -74,20 +82,20 @@ export class CreateComponent implements OnInit {
         modelName: ['', Validators.required],
         api_version: ['', Validators.required],
         isAzureOpenAI: [true],
-        customModelName: ['', this.customModelValidator],
+        customModelName: [''],
       }),
       consolidatedQueryModel: this._formBuilder.group({
         modelName: ['', Validators.required],
         api_version: ['', Validators.required],
         isAzureOpenAI: [true],
-        customModelName: ['', this.customModelValidator],
+        customModelName: [''],
         temperature: [0]
       }),
       finetunedModel: this._formBuilder.group({
-        modelName: ['', Validators.required],
-        api_version: ['', Validators.required],
+        modelName: [],
+        api_version: [],
         isAzureOpenAI: [true],
-        customModelName: ['', this.customModelValidator],
+        customModelName: [],
         temperature: [0]
       }),
       followupModel: this._formBuilder.group({
@@ -108,8 +116,8 @@ export class CreateComponent implements OnInit {
         modelName: [],
         api_version: [],
         isAzureOpenAI: [true],
-        customModelName: [],
-        temperature: [0]
+        temperature: [0],
+        customModelName: []
       })
     })
     this.searchPreferenceForm = this._formBuilder.group({
@@ -117,15 +125,18 @@ export class CreateComponent implements OnInit {
     });
 
   }
-
-  customModelValidator(control: FormControl): ValidationErrors | null {
-    const modelName = control?.parent?.get('modelName')?.value;
-    if (modelName === 'Add Custom Model') {
-      return Validators.required(control);
+  updateCustomValidity(selectedValue: string, formGroup: string) {
+    const control = this.llmSpecification.get(formGroup)?.get('customModelName')
+    if (selectedValue === 'Add Custom Model') {
+      control?.setValidators([Validators.required]);
     }
-    return null;
-  }
+    else {
+      control?.clearValidators();
+    }
+    control?.updateValueAndValidity();
+    console.log(control);
 
+  }
   add(event: MatChipInputEvent, array: Item[]): void {
     const value = (event.value || '').trim();
     if (value) {
@@ -186,25 +197,35 @@ export class CreateComponent implements OnInit {
   }
 
   updateValidators(event: any, formGroupName: string) {
-    formGroupName = formGroupName.replace('Form', '')
-    const controlNames = ['customModelName', 'modelName', 'api_version'];
+    const controlNames = ['modelName', 'api_version'];
     const formGroup = this.llmSpecification?.get(formGroupName);
 
-    if (event.checked) {
-      this.availableModels.push(formGroupName);
+    if (event) {
+      if (!this.availableModels.includes(formGroupName)) {
+        this.availableModels.push(formGroupName);
+      }
+    } else {
+      const index = this.availableModels.indexOf(formGroupName);
+      if (index !== -1) {
+        this.availableModels.splice(index, 1);
+      }
     }
-    else this.availableModels.splice(this.availableModels.indexOf(formGroupName), 1);
 
     controlNames.forEach((controlName) => {
       const control = formGroup?.get(controlName);
-      if (event.checked) {
+
+      if (event) {
         control?.setValidators([Validators.required]);
       } else {
         control?.clearValidators();
       }
+
       control?.updateValueAndValidity();
     });
+
+    console.log(this.llmSpecification);
   }
+
   done() {
     const formValues = this.readFormGroups({
       pineconeCredentials: this.pineconeCredentials,
@@ -245,7 +266,6 @@ export class CreateComponent implements OnInit {
       prompts
     };
     this.finalJSON = JSON.stringify(this.finalIndexJSON, null, 2);
-
   }
 
   readFormGroups(formGroups: any) {
@@ -265,7 +285,6 @@ export class CreateComponent implements OnInit {
     if (event.selectedIndex == 4) {
       this.done();
     }
-
   }
   createIndex() {
     this._snackBar.open("Index Created Successfully", 'close', {
@@ -292,5 +311,46 @@ export class CreateComponent implements OnInit {
 
     //   }
     // )
+  }
+
+  addCustomModel(modelName: string): void {
+    this.availableCustomModels = this.availableCustomModels.filter(model => model !== modelName);
+    if (modelName === "Fallback Model") {
+      this.updateValidators(true, 'fallbackModel');
+      this.showFallbackModel = true;
+    }
+    else if (modelName === "Finetune Model") {
+      this.updateValidators(true, 'finetunedModel');
+      this.showFinetuneModel = true;
+    }
+    else if (modelName === "Followup Model") {
+      this.updateValidators(true, 'followupModel');
+      this.showFollowupModel = true;
+      this.availableOptions.push('FOLLOW_UP_QUERY_PROMPT')
+    }
+    else {
+      this.updateValidators(true, 'presentationModel')
+      this.showPresentationModel = true;
+    }
+  }
+  removeModelPanel(modelName: string): void {
+    this.availableCustomModels.push(modelName);
+    if (modelName === "Fallback Model") {
+      this.updateValidators(false, 'fallbackModel');
+      this.showFallbackModel = false;
+    }
+    else if (modelName === "Finetune Model") {
+      this.updateValidators(false, 'finetunedModel');
+      this.showFinetuneModel = false;
+    }
+    else if (modelName === "Followup Model") {
+      this.updateValidators(false, 'followupModel');
+      this.showFollowupModel = false;
+      this.availableOptions = this.availableOptions.filter(option => option !== 'FOLLOW_UP_QUERY_PROMPT')
+    }
+    else {
+      this.updateValidators(false, 'presentationModel');
+      this.showPresentationModel = false;
+    }
   }
 }
--- src/app/services/API-services/app.service.ts
+++ src/app/services/API-services/app.service.ts
@@ -6,21 +6,19 @@ import { Injectable } from '@angular/core';
 export class AppService {
   constructor(private http: HttpClient) { }
   mockapi: string = "https://6380f4bf786e112fe1bf0f9a.mockapi.io/getData"
-
+  url = 'https://retailassist-poc.kore.ai/semanticSearch/getAvaibleIndexProperties';
+  httpOptions = {
+    headers: new HttpHeaders({
+      'Content-Type': 'application/json',
+      "Authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJib3RJZCI6InN0LWQwMjc4ZjE4LTIyMjgtNWQ2Zi04MjUwLWEyZTJiMTRjYmQyMSIsInByb2plY3RBY2Nlc3MiOiJTRU1BTlRJQ19TRUFSQ0giLCJpYXQiOjE2OTIyNTQzNzB9.srlH3yS76oO0k3V_sBiN58U-WzYETg2_YW70_cyz7RQ",
+      "Stage": "Dev"
+    })
+  };
   getData(): any {
-    const url = 'https://retailassist-poc.kore.ai/semanticSearch/getAvaibleIndexProperties';
-
-    // const localhost = "http://localhost:3016/semanticSearch/getAvaibleIndexProperties"
-
-    const httpOptions = {
-      headers: new HttpHeaders({
-        'Content-Type': 'application/json',
-        "Authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJib3RJZCI6InN0LWQwMjc4ZjE4LTIyMjgtNWQ2Zi04MjUwLWEyZTJiMTRjYmQyMSIsInByb2plY3RBY2Nlc3MiOiJTRU1BTlRJQ19TRUFSQ0giLCJpYXQiOjE2OTIyNTQzNzB9.srlH3yS76oO0k3V_sBiN58U-WzYETg2_YW70_cyz7RQ",
-        "Stage": "Dev"
-      })
-    };
-    return this.http.get(this.mockapi, httpOptions).pipe(map((data: any) => {
+    return this.http.get(this.mockapi, this.httpOptions).pipe(map((data: any) => {
       return data[0];
+      if (data?.length == 0) return data[0];
+      return data;
     }));
   }
 
